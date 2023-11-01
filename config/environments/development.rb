770f15bb377827403bc1c372a30423451b903dc7
ke-sato <keta0616@icloud.com>
2023-11-01 14:55:50 +0900

    Dockerfile:編集

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,4 +73,8 @@ Rails.application.configure do
 
   # Raise error when a before_action's only/except options reference missing actions
   config.action_controller.raise_on_missing_callback_actions = true
+
+  # 追加
+  config.file_watcher = ActiveSupport::FileUpdateChecker
+  config.reload_classes_only_on_change = true
 end
